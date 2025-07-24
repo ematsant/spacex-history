cc83ee2a7fe8d447c3ba313120bfe88e3caeee24
ematsant <ematsant@gmail.com>
2025-07-24 00:16:19 -0300

    fix: display correct launch date from v5 api

--- src/components/LaunchItem.js
+++ src/components/LaunchItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Image, Text, Flex, Tag, Spacer } from '@chakra-ui/react';
-import defaultImage from '../assets/default-rocket.png'; // Vou adicionar essa imagem depois
+import defaultImage from '../assets/default-rocket.png'; 
 
 export function LaunchItem({ launch }) {
   return (
@@ -14,8 +14,8 @@ export function LaunchItem({ launch }) {
       <Flex>
         <Box>
           <Flex align="baseline">
-            <Tag size="sm" colorScheme={launch.launch_success ? "green" : "red"}>
-              {launch.launch_success ? "Success" : "Failure"}
+            <Tag size="sm" colorScheme={launch.success ? "green" : "red"}>
+              {launch.success ? "Success" : "Failure"}
             </Tag>
             <Box
               color="gray.500"
@@ -37,19 +37,19 @@ export function LaunchItem({ launch }) {
             isTruncated
             fontSize="xl"
           >
-            {launch.mission_name}
+            {launch.name}
           </Text>
 
           <Text fontSize="sm" color="gray.600">
-            {new Date(launch.launch_date_local).toLocaleDateString("pt-BR")}
+            {new Date(launch.date_utc).toLocaleDateString("pt-BR")}
           </Text>
         </Box>
         <Spacer />
         <Image
-            boxSize="100px"
-            objectFit="contain"
-            src={launch.links.mission_patch_small || defaultImage}
-            alt={`Patch for ${launch.mission_name}`}
+          boxSize="100px"
+          objectFit="contain"
+          src={launch.links.patch.small || defaultImage}
+          alt={`Patch for ${launch.name}`}
         />
       </Flex>
     </Box>
